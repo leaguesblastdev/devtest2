4a012e085cd0244cabdcb6b68ec7a2de89093370
root <root@vmi361642.contaboserver.net>
2020-12-30 18:01:08 +0100

    add watch mode true

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@
          instances: 4 ,
 	 exec_mode: cluster,
          autorestart: true,
-         watch: false,
+         watch: true,
          max_memory_restart: '512M',
          env: {
            NODE_ENV: 'development',
